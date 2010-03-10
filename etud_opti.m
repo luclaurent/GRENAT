ad0064231b6c3d6c619d701471a68e6c943d98d5
Luc <luc.laurent@ens-cachan.fr>
2010-03-10 17:36:35 +0100

    ajout Kirgeage

--- etud_opti.m
+++ etud_opti.m
@@ -1,7 +1,7 @@
 %Fichier d'étude et de mise en oeuvre des démarche de la biblio
 %L LAURENT   --  31/01/2010   --  luc.laurent@ens-cachan.fr
 clf;clc;close all; clear all;
-addpath('doe/lhs');
+addpath('doe/lhs');addpath('dace');
 
 %%Définition de l'espace de conception
 xmin=-2;
@@ -15,21 +15,28 @@ pas=0.1;
 x=xmin:pas:xmax;
 y=ymin:pas:ymax;
 [X,Y]=meshgrid(x,y);
-Z=rosenbrock(X,Y);
+%Z=rosenbrock(X,Y);
+Z=peaks(X,Y);
 figure;
 surfc(X,Y,Z)
-shading interp
+
+hlight=light;              % activ. éclairage
+lighting('gouraud')        % type de rendu
+lightangle(hlight,48,70) % dir. éclairage
+%shading interp
+
 
 %% Tirages: plan d'expérience
 disp('===== DOE =====');
 %nb d'échantillons
-nb_samples=25;
+nb_samples=500;
 %LHS uniform
 xmin=[xmin,ymin];
 xmax=[xmax,ymax];
 tirages=lhsu(xmin,xmax,nb_samples);
 %évaluations aux points
-eval=rosenbrock(tirages(:,1),tirages(:,2));
+%eval=rosenbrock(tirages(:,1),tirages(:,2));
+eval=peaks(tirages(:,1),tirages(:,2));
 
 
 %% Génération du métamodèle
@@ -37,6 +44,135 @@ disp('===== METAMODELE =====');
 %type d'interpolation
 %PRG: regression polynomiale
 
-meta.type='PRG';
+meta.type='KRG';
+%degré de linterpolation/regression (si nécessaire)
+meta.deg=4;
+%paramètre Krigeage
+meta.theta=0.5;
+meta.regr='regpoly2';
+meta.corr='corrgauss';
 
-meta_prg(tirages,eval);
+
+switch meta.type
+    case 'PRG'
+
+[coef,MSE]=meta_prg(tirages,eval,meta.deg);
+disp(MSE)
+ZRG=polyrg(coef,X,Y,meta.deg);
+
+
+
+
+
+
+%%%affichage des surfaces
+figure;
+hold on
+surfc(X,Y,ZRG)
+hlight=light;
+lighting('gouraud')
+lightangle(hlight,48,70) % dir. éclairage
+hold on
+plot3(tirages(:,1),tirages(:,2),eval,'.','MarkerEdgeColor','g',...
+                'MarkerFaceColor','g',...
+                'MarkerSize',30)
+title('Surface obtenue par regression polynomiale');
+view(3)
+
+figure;
+hold on
+surf(X,Y,Z,'FaceColor','white','EdgeColor','blue')
+hold on
+surf(X,Y,ZRG,'FaceColor','white','EdgeColor','red')
+hlight=light;
+lighting('gouraud')
+lightangle(hlight,48,70) % dir. éclairage
+view(3)
+
+
+figure;
+for ii=2
+    meta.deg=ii;
+
+[coef,MSE]=meta_prg(tirages,eval,meta.deg);
+disp(MSE)
+ZRG=polyrg(coef,X,Y,meta.deg);
+
+hold on
+surf(X,Y,ZRG)
+%colormap hsv
+end
+%hlight=light;
+%lighting('gouraud')
+%lightangle(hlight,48,70) % dir. éclairage
+%hold on
+%plot3(tirages(:,1),tirages(:,2),eval,'.','MarkerEdgeColor','g',...
+%                'MarkerFaceColor','g',...
+%                'MarkerSize',30)
+%title('Surface obtenue par regression polynomiale');
+xlabel('x_{1}')
+ylabel('x_{2}')
+zlabel('F')
+
+view(3)
+
+figure;
+for ii=4
+    meta.deg=ii;
+
+[coef,MSE]=meta_prg(tirages,eval,meta.deg);
+disp(MSE)
+ZRG=polyrg(coef,X,Y,meta.deg);
+
+hold on
+surf(X,Y,ZRG)
+%colormap hsv
+end
+%hlight=light;
+%lighting('gouraud')
+%lightangle(hlight,48,70) % dir. éclairage
+%hold on
+%plot3(tirages(:,1),tirages(:,2),eval,'.','MarkerEdgeColor','g',...
+%                'MarkerFaceColor','g',...
+%                'MarkerSize',30)
+%title('Surface obtenue par regression polynomiale');
+xlabel('x_{1}')
+ylabel('x_{2}')
+zlabel('F')
+view(3)
+
+    case 'KRG' %utilisation de la Toolbox DACE
+        [model,perf]=dacefit(tirages,eval,meta.regr,meta.corr,meta.theta);
+        Zk=zeros(size(X));
+        for ii=1:size(X,1)
+            for jj=1:size(X,2)
+                ZK(ii,jj)=predictor([X(ii,jj) Y(ii,jj)],model);
+            end
+        end
+            %%%affichage des surfaces
+        figure;
+        hold on
+        mesh(x,y,ZK)
+        hlight=light;
+        lighting('gouraud')
+        lightangle(hlight,48,70) % dir. éclairage
+        hold on
+        plot3(tirages(:,1),tirages(:,2),eval,'.','MarkerEdgeColor','g',...
+                        'MarkerFaceColor','g',...
+                        'MarkerSize',30)
+        title('Surface obtenue par regression polynomiale');
+        view(3)
+
+        figure;
+        hold on
+        surf(X,Y,Z,'FaceColor','white','EdgeColor','blue')
+        hold on
+        surf(X,Y,ZK,'FaceColor','white','EdgeColor','red')
+        hlight=light;
+        lighting('gouraud')
+        lightangle(hlight,48,70) % dir. éclairage
+        view(3)
+end
+        
+
+   

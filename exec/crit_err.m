1275114a377f7cb782be870056eea497b8d84b95
Luc <luc.laurent@ens-cachan.fr>
2011-02-03 10:49:30 +0100

    modif affichage critères si non présence des données la réponse de référence

--- exec/crit_err.m
+++ exec/crit_err.m
@@ -3,20 +3,22 @@
 
 function err=crit_err(Zap,Zref,krg)
 
+if ~isempty(Zref)
+    err.emse=mse_p(Zref,Zap);
+    err.r2=r_square(Zref,Zap);
+    err.eraae=raae(Zref,Zap);
+    err.ermae=rmae(Zref,Zap);
+    [err.eq1,err.eq2,err.eq3]=qual(Zref,Zap);
+    fprintf('\nMSE= %6.4d\n',err.emse);
+    fprintf('R2= %6.4d\n',err.r2);
+    fprintf('RAAE= %6.4d\n',err.eraae);
+    fprintf('RMAE= %6.4d\n',err.ermae);
+    fprintf('Q1= %6.4d,  Q2= %6.4d,  Q3= %6.4d\n\n',err.eq1,err.eq2,err.eq3);
+else
+    err=[];
+end
 
-err.emse=mse_p(Zref,Zap);
-err.r2=r_square(Zref,Zap);
-err.eraae=raae(Zref,Zap);
-err.ermae=rmae(Zref,Zap);
-[err.eq1,err.eq2,err.eq3]=qual(Zref,Zap);
-fprintf('\nMSE= %6.4d\n',err.emse);
-fprintf('R2= %6.4d\n',err.r2);
-fprintf('RAAE= %6.4d\n',err.eraae);
-fprintf('RMAE= %6.4d\n',err.ermae);
-fprintf('Q1= %6.4d,  Q2= %6.4d,  Q3= %6.4d\n\n',err.eq1,err.eq2,err.eq3);
-
-
-if isfield(krg,'cv')    
+if isfield(krg,'cv')
     fprintf('\n>>>Validation croisee<<<\n');
     fprintf('Biais moyen=%g\n',krg.cv.bm);
     fprintf('MSE=%g\n',krg.cv.msep);

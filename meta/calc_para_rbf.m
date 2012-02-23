0992ad90d37d01e98adfc05a2b923506908ddbab
L2 <laurent@lmt.ens-cachan.fr>
2012-02-23 16:40:41 +0100

    procédure de calcul des paramètres des fonctions RBF à partir des résultats empiriques (Franke/Hardy)

---
+++ meta/calc_para_rbf.m
@@ -0,0 +1,8 @@
+%% procédure de calcul automatique du paramètre RBF
+% L. LAURENT -- 23/02/2012 -- laurent@lmt.ens-cachan.fr
+
+function para=calc_para_rbf(tirages)
+
+% choix de la stratégie
+type='Hardy'; %Hardy/Franke
+

4b3a024b80dc661554366db9aa73bb4cdcaaf27f
l2 <l2@crans.org>
2012-06-27 11:03:18 +0200

    réécriture fonction test pour mode démonstration + extraction informations (minimums locaux et globaux)

--- fct/fct_test/fct_beale.m
+++ fct/fct_test/fct_beale.m
@@ -4,9 +4,9 @@
 %minimum global: f(x1,x2)=0 pour (x1,x2)=(3,-0.5)
 
 %Domaine d'etude de la fonction: -4.5<x1<4.5, -4.5<x<4.5 
-function [p,dp]=fct_beale(xx)
+function [p,dp,infos]=fct_beale(xx,dim)
 
-
+if ~isempty(xx)
 if size(xx,3)>2
     error('La fonction Beale est une fonction de deux variables');
 elseif size(xx,3)==1
@@ -32,5 +32,19 @@ if nargout==2
     dp(:,:,2)=2*xxx.*(1.5-xxx+xxx.*yyy)+...
         4*xxx.*yyy.*(2.25 - xxx + xxx.*yyy.^2) +...
         6*xxx.*yyy.^2.*(2.625 - xxx + xxx.*yyy.^3);
+end
+else
+    if nargin==2
+        nbvar=dim;
+    end
+    p=[];
+    dp=[];
+end
+%sortie informations sur la fonction
+if nargout==3
+    infos.min_glob.Z=0;
+    infos.min_glob.X=[3 0.5];
+    infos.min_loc.Z=0;
+    infos.min_loc.X=[3 0.5];
 end
 end

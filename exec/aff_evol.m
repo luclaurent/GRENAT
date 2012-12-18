6b3868e168152cb76b1163c30a8cdd0f3e79f236
l2 <l2@crans.org>
2012-12-18 15:53:43 +0100

    modification affichage évolution des grandeurs optimisation

--- exec/aff_evol.m
+++ exec/aff_evol.m
@@ -67,36 +67,66 @@ else
     %set(id_graph,'LineStyle','None')
     xdat=get(id_graph,'Xdata');
     ydat=get(id_graph,'Ydata');
+    
     if iscell(xdat)
         newX=[xdat{1} X];
         newY=[ydat{1} Y];
+        yy=horzcat(ydat{:});
+        maxY=max(yy(:));
+        minY=min(yy(:));
+        xx=horzcat(xdat{:});
+        maxX=max(xx(:));
+        minX=min(xx(:));
     else
         newX=[xdat X];
         newY=[ydat Y];
+        maxY=max(max(ydat));
+        minY=min(min(ydat));
+        maxX=max(max(xdat));
+        minX=min(min(xdat));
     end
-    %Mise a jour du graphe    
+    %Mise a jour du graphe
     idd=feval(type,id_plot,newX,newY,'k');
     %hold on
     %affectation nom au graphe
     set(idd,'Tag',tag);
     plot(id_plot,newX,newY,'ok','MarkerSize',2);
-    
+    ext_bornes_x=get(id_plot,'xlim');
+    maxX=max([maxX,newX,ext_bornes_x]);
+    minX=min([minX,newX,ext_bornes_x]);
     %trace de la cible
     if ~isempty(cible)
-        warning off all
-        ext_bornes_x=get(id_plot,'xlim');
-        plot(id_plot,ext_bornes_x,cible*ones(1,2),'LineWidth',2,'Color','r');
+        maxY=max([maxY,cible]);
+        minY=min([minY,cible]);
+        warning off all        
+        plot(id_plot,[minX maxX],cible*ones(1,2),'LineWidth',2,'Color','r');
         ext_bornes_y=get(id_plot,'ylim');
         new_bornes_y=ext_bornes_y;
         if ext_bornes_y(1)==cible
+            if ech_log
+                new_bornes_y(1)=new_bornes_y(1)-eps;
+            else
             new_bornes_y(1)=new_bornes_y(1)-0.2*abs(new_bornes_y(2)-new_bornes_y(1));
-            ylim(id_plot,new_bornes_y)
+            end
         elseif ext_bornes_y(2)==cible
-            new_bornes_y(2)=new_bornes_y(2)+0.2*abs(new_bornes_y(2)-new_bornes_y(1));
-            ylim(id_plot,new_bornes_y)
+            if ech_log
+                new_bornes_y(2)=new_bornes_y(2)+eps;
+            else                
+                new_bornes_y(2)=new_bornes_y(2)+0.2*abs(new_bornes_y(2)-new_bornes_y(1));
+            end
         end
         warning on all
+    else
+        new_bornes_y=[];
     end
+    if ech_log
+        new_bornes_y=new_bornes_y(new_bornes_y>0);
+    end
+    maxY=max([maxY,newY,new_bornes_y]);
+    minY=min([minY,newY,new_bornes_y]);
+
+    ylim(id_plot,[minY maxY])
+    xlim(id_plot,[minX maxX])
     xlabel(id_plot,labelx)
     ylabel(id_plot,labely)
     title(id_plot,titre)

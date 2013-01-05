8ddac5f72a9971929c04700c1f6db43baf6b6702
l2 <l2@crans.org>
2013-01-05 23:49:19 +0100

    nouvelles stratégie de détermination des paramètres pour KRG/CKRG

--- meta/estim_para_krg_ckrg.m
+++ meta/estim_para_krg_ckrg.m
@@ -30,7 +30,7 @@ else
     nb_para_optim=nb_para;
 end
 % traitement des cas particuliers (fonctions de correlation exponentielles
-% g�n�ralis�es)
+% generalisees)
 switch meta.corr
     case 'corr_expg'
         %definition des bornes de l'espace de recherche
@@ -129,6 +129,78 @@ switch meta.para.method
         if ~aff_warning;warning on all;end
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    case 'tir_min'
+        nb_pts=nb_para*50;
+        type_tir='LHS';
+        fprintf('||Tir_min||  Tirage %s de %i points\n',type_tir,nb_pts);
+        doePop.Xmin=lb;doePop.Xmax=ub;doePop.nb_samples=nb_pts;doePop.aff=false;doePop.type=type_tir;
+        tir_pop=gene_doe(doePop);
+        crit=zeros(1,nb_pts);
+        parfor tir=1:nb_pts
+            crit(tir)=fun(tir_pop(tir,:));
+        end
+        [fval,IX]=min(crit);
+        x=tir_pop(IX,:);
+        
+        %stockage retour algo
+        para_estim.out_algo.fval=fval;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    case 'tir_min_fmincon'
+        nb_pts=nb_para*10;
+        type_tir='LHS';
+        fprintf('||Tir_min + opti||  Tirage %s de %i points\n',type_tir,nb_pts);
+        doePop.Xmin=lb;doePop.Xmax=ub;doePop.nb_samples=nb_pts;doePop.aff=false;doePop.type=type_tir;
+        tir_pop=gene_doe(doePop);
+        crit=zeros(1,nb_pts);
+        parfor tir=1:nb_pts
+            crit(tir)=fun(tir_pop(tir,:));
+        end
+        [fval1,IX]=min(crit);
+        x1=tir_pop(IX,:);
+        %recherche locale
+        fprintf('||Fmincon (IP)|| Initialisation au point:\n');
+        fprintf('%g ',x1); fprintf('\n');
+        [x,fval2,exitflag,output] = fmincon(fun,x1,[],[],[],[],lb,ub,[],options_fmincon);
+        %stockage retour algo
+        para_estim.out_algo.fval1=fval1;
+        para_estim.out_algo=output;
+        para_estim.out_algo.fval=fval2;
+        para_estim.out_algo.exitflag=exitflag;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    case 'tir_min_sqp'
+        nb_pts=nb_para*10;
+        type_tir='LHS';
+        fprintf('||Tir_min + opti||  Tirage %s de %i points\n',type_tir,nb_pts);
+        doePop.Xmin=lb;doePop.Xmax=ub;doePop.nb_samples=nb_pts;doePop.aff=false;doePop.type=type_tir;
+        tir_pop=gene_doe(doePop);
+        crit=zeros(1,nb_pts);
+        parfor tir=1:nb_pts
+            crit(tir)=fun(tir_pop(tir,:));
+        end
+        [fval1,IX]=min(crit);
+        x1=tir_pop(IX,:);
+        %recherche locale
+        fprintf('||Fincon (SQP)|| Initialisation au point:\n');
+        fprintf('%g ',x1); fprintf('\n');
+        options_fmincon=optimset(options_fmincon,'Algorithm','sqp');
+        [x,fval2,exitflag,output] = fmincon(fun,x1,[],[],[],[],lb,ub,[],options_fmincon);
+        %stockage retour algo
+        para_estim.out_algo.fval1=fval1;
+        para_estim.out_algo=output;
+        para_estim.out_algo.fval=fval2;
+        para_estim.out_algo.exitflag=exitflag;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     case 'fminbnd'
         fprintf('||Fminbnd|| Initialisation au point:\n');
         fprintf('%g ',x0); fprintf('\n');
@@ -157,7 +229,8 @@ switch meta.para.method
         para_estim.out_algo.exitflag=exitflag;
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     case 'fmincon'
         fprintf('||Fmincon|| Initialisation au point:\n');
         fprintf('%g ',x0); fprintf('\n');
@@ -226,6 +299,10 @@ switch meta.para.method
             end
         end
         if ~aff_warning;warning on all;end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     case 'ga'
         fprintf('||Ga|| Initialisation par tirages %s:\n',popInitManu);
         if ~aff_warning;warning off all;end

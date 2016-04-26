4cf9d658a778cabae6513270cb86bea0f4f6d5dd
Luc <l2@crans.org>
2016-04-26 18:32:20 +0200

    Writing and translate function for preparing data to Indirect-Gradient approaches

--- src/surrogate/PrepIn.m
+++ src/surrogate/PrepIn.m
@@ -11,136 +11,132 @@ fprintf('>>> Preparation of data for Indirect Gradient-Based Surrogate Models  \
 %dimension of the problem (number of parameters)
 np=size(samplingIn,2);
 % initial number of sample points
-nbs_init=size(samplingIn,1);
+ns_init=size(samplingIn,1);
 
+%if missing data is considered
 if ~nargin==5
     missData.resp.on=false;
     missData.grad.on=false;
 end  
 
-%en fonction du type de donnees en entree
+%with respect with the kind of gradient data
 if ~isstruct(gradIn)
-    fprintf('>> Valeur pas developpement de Taylor (manu):')
-    fprintf(' %d',meta.para.pas_tayl);
+    fprintf('>> Step of the Taylor expansion (manu):')
+    fprintf(' %d',meta.para.step_taylor);
     fprintf('\n')
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%% Creation des nouveaux points (1 nouveau points par direction)
-    %traitement pas Taylor
-    if numel(meta.para.pas_tayl)~=np
-        pas_tayl=meta.para.pas_tayl(1)*ones(1,np);
+    %%% Create new points (1 per direction)
+    %manage Taylor's step
+    if numel(meta.para.step_taylor)~=np
+        stepTaylor=meta.para.step_taylor(1)*ones(1,np);
     else
-        pas_tayl=meta.para.pas_tayl;
+        stepTaylor=meta.para.step_taylor;
     end
     
-    %Reordonnancement tirages et duplication
-    reord_tir=reshape(samplingIn',1,[]);
-    dup_tir=repmat(reord_tir,np+1,[]);
-    %creation decalage par direction
-    mat_pas=diag(pas_tayl);
-    mat_pas_dup=[zeros(1,np*nbs_init);repmat(mat_pas,1,nbs_init)];
-    badord_tir=dup_tir+mat_pas_dup;
-    tirages_new=zeros((np+1)*nbs_init,np);
+    %Reordering sampled points and duplicates
+    reordS=reshape(samplingIn',1,[]);
+    dupS=repmat(reordS,np+1,[]);
+    %create shift per direction
+    matStep=diag(stepTaylor);
+    matStepDup=[zeros(1,np*ns_init);repmat(matStep,1,ns_init)];
+    badordS=dupS+matStepDup;
+    newSampling=zeros((np+1)*ns_init,np);
         
-    %nouveaux points
+    %new points
     for ii=1:np
-        li=ii:np:(np*nbs_init);            
-        tirages_new(:,ii)=reshape(badord_tir(:,li),(np+1)*nbs_init,[]);
+        li=ii:np:(np*ns_init);            
+        newSampling(:,ii)=reshape(badordS(:,li),(np+1)*ns_init,[]);
     end
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%% Nettoyage si donnees manquantes (attention en cas de reponse
-    %%% manquante, on retire egalement le gradient car impossible d'estimer
-    %%% la valeur de la reponse aux points proches sans la valeur de cette
-    %%% reponse)
-    pos_ev=[];
-    if missData.eval.on
+    %%% Cleaning if missing data (Caution in the case of missing response, the Nettoyage si donnees manquantes (attention en cas de reponse
+    %%% the associated gradients is also removed because it will be unable
+    %%% to estimate the response without this value)
+    posR=[];
+    if missData.resp.on
         pos_tmp=missData.eval.ix_manq;
-        fprintf(' >>> Supression informations (r�ponse(s) manquante(s)) a(aux) point(s):');
-        fprintf(' %i',pos_ev);
+        fprintf(' >>> Remove information (missing data at point(s):');
+        fprintf(' %i',posR);
         fprintf('\n');
-        %renumerotation pour extraction bonnes valeurs
+        %renumber for extracting right values
         pos_tmp=(pos_tmp-1)*(np+1)+1;
         for ii=1:numel(pos_tmp)
-            pos_ev=[pos_ev pos_tmp(ii):pos_tmp(ii)+np+1];
+            posR=[posR pos_tmp(ii):pos_tmp(ii)+np+1];
         end
     end
 
-    pos_gr=[];
+    posG=[];
     if missData.grad.on
-        pos_tmp=missData.grad.ix_manq;
-        pos_gr=(pos_tmp(:,1)-1)*(np+1)+1+pos_tmp(:,2);
+        pos_tmp=missData.grad.ix_miss;
+        posG=(pos_tmp(:,1)-1)*(np+1)+1+pos_tmp(:,2);
     end
   
     %position des elements manquants
-    pos_manq=unique([pos_ev,pos_gr]);
+    pos_manq=unique([posR,posG]);
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%% Creation des nouvelles reponses (aux points ajoutes)
+    %%% Create new responses (at added points)
     
-    %Reordonnancement reponses
-    tmp_ev=[respIn zeros(nbs_init,np-1)];
-    reord_ev=reshape(tmp_ev',1,[]);
-    dup_ev=repmat(reord_ev,np+1,[]);
-    %Reordonnancement gradients
+    %Reordering responses
+    tmpR=[respIn zeros(ns_init,np-1)];
+    reordR=reshape(tmpR',1,[]);
+    dup_ev=repmat(reordR,np+1,[]);
+    %Reordering gradients
     reord_grad=reshape(gradIn',1,[]);
     dup_grad=repmat(reord_grad,np+1,[]);
-    tmp=mat_pas_dup.*dup_grad;
-    %si donnees manquantes, traitement specifiques pour eviter les NaN    
-    if missData.grad.on||missData.eval.on
+    tmp=matStepDup.*dup_grad;
+    %if missing data, sepcific proces for avoiding NaN
+    if missData.grad.on||missData.resp.on
              IX=find(isnan(tmp(:)));
              tmp(IX)=0;
     end
         badord_ev=dup_ev+tmp;
-    %nouvelles reponses
-    tmp_ev=zeros((np+1)*nbs_init,np);
+    %new responses
+    tmpR=zeros((np+1)*ns_init,np);
     for ii=1:np
-        li=ii:np:(np*nbs_init);
-        tmp_ev(:,ii)=reshape(badord_ev(:,li),(np+1)*nbs_init,[]);
+        li=ii:np:(np*ns_init);
+        tmpR(:,ii)=reshape(badordS(:,li),(np+1)*ns_init,[]);
     end
-    eval_new=sum(tmp_ev,2);
+    newResp=sum(tmpR,2);
     
-    %supression des donnees manquantes
+    %removes missing data
     if ~isempty(pos_manq)
-        tirages_new(pos_manq,:)=[];
-        eval_new(pos_manq)=[];
+        newSampling(pos_manq,:)=[];
+        newResp(pos_manq)=[];
     end
 
 else
-    %%Attention donnees manquante non prises en compte dans cette approche
-    %%(a coder)
-    %calcul des pas de Taylor dans chaque direction
-    pas_tayl_d=gradIn.tirages{1}-repmat(samplingIn(1,:),np,1);
-    pas_tayl=abs(sum(pas_tayl_d,2));
+    %%Be careful missing data not taking into account in this part
+    %%(to be coded)
+    %compute Taylor's step in each direction
+    stepTaylorD=gradIn.tirages{1}-repmat(samplingIn(1,:),np,1);
+    stepTaylor=abs(sum(stepTaylorD,2));
     
     %Nouveaux tirages et reponses
-    tirages_new=zeros((np+1)*nbs_init,np);
-    eval_new=zeros((np+1)*nbs_init,1);
-    for ii=1:nbs_init
+    newSampling=zeros((np+1)*ns_init,np);
+    newResp=zeros((np+1)*ns_init,1);
+    for ii=1:ns_init
         li_tir=(ii-1)*(np+1)+1;
         li_tirg=li_tir+1:ii*(np+1);
-        tirages_new(li_tir,:)=samplingIn(ii,:);
-        tirages_new(li_tirg,:)=gradIn.tirages{ii};
-        eval_new(li_tir)=respIn(ii);
-        eval_new(li_tirg)=gradIn.eval{ii};
+        newSampling(li_tir,:)=samplingIn(ii,:);
+        newSampling(li_tirg,:)=gradIn.tirages{ii};
+        newResp(li_tir)=respIn(ii);
+        newResp(li_tirg)=gradIn.eval{ii};
     end
     
-    fprintf('>> Valeur pas developpement de Taylor (auto):')
-    fprintf(' %d',pas_tayl);
+    fprintf('>> Step of the Taylor expansion (auto):')
+    fprintf(' %d',stepTaylor);
     fprintf('\n')
     
 end
- fprintf('\n');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Construction Krigeage a partir de ces donnees
-fprintf('>>> Nombre de points pris en compte: %d  \n',numel(eval_new));
-ret=meta_krg_ckrg(tirages_new,eval_new,[],meta);
-%% extraction informations
-ret.in.tirages_init=samplingIn;
-ret.in.eval_init=respIn;
-ret.in.pas_tayl=pas_tayl;
-
+%store and extract information
+ret.init.sampling=samplingIn;
+ret.init.resp=respIn;
+ret.init.grad=gradIn;
+ret.new.sampling=newSampling;
+ret.new.resp=newResp;
+ret.stepTaylor=stepTaylor;

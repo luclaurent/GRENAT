8d85759c140e8f7a9ecbe922dfb316b43ea8f9d6
Luc <l2@crans.org>
2016-04-28 15:49:54 +0200

    add optional parameters for displaying only date or time

--- src/disp/dispDate.m
+++ src/disp/dispDate.m
@@ -1,8 +1,26 @@
 % Display date and time
 % L. LAURENT -- 17/12/2010 -- luc.laurent@lecnam.net
 
+function dispDate(type)
+
 day=clock;
-fprintf('=============================================\n');
-fprintf('Date: %d/%d/%d   Time: %02.0f:%02.0f:%02.0f\n',...
-    day(3), day(2), day(1), day(4), day(5), day(6));
-fprintf('=============================================\n');
+if nargin==0
+    fprintf('=============================================\n');
+    fprintf('Date: %d/%d/%d   Time: %02.0f:%02.0f:%02.0f\n',...
+        day(3), day(2), day(1), day(4), day(5), day(6));
+    fprintf('=============================================\n');
+else
+    switch type
+        case 'date'
+            fprintf('==============\n');
+            fprintf('Date: %d/%d/%d\n',...
+                day(3), day(2), day(1),);
+            fprintf('==============\n');
+        case 'time'
+            fprintf('===============\n');
+            fprintf('Time: %02.0f:%02.0f:%02.0f\n',...
+                day(4), day(5), day(6));
+            fprintf('===============\n');
+    end
+end
+end

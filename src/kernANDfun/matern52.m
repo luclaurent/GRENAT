936ce841d0114e4bee791bbbb2fb95c2f8e7e808
Luc <l2@crans.org>
2016-05-19 17:26:27 +0200

    correction on the matern 5/2 function (small mistake)

--- src/kernANDfun/matern52.m
+++ src/kernANDfun/matern52.m
@@ -19,7 +19,7 @@ lP=1./para(:,1);
 
 %compute value of the function at point xx
 etd=exp(-abs(xx)./lP*sqrt(5));
-co=1-abs(xx)./lP*sqrt(5)+sqrt(5).*xx.^2./(3*lP);
+co=1+abs(xx)./lP*sqrt(5)+sqrt(5).*xx.^2./(3*lP);
 k=co.*etd;
 
 %compute first derivatives

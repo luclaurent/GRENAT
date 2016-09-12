7c62fbda44ee6908db7bec29f1d1c30925b71bc0
Luc <l2@crans.org>
2016-09-12 15:17:33 +0200

    add function for displaying text in command window with a keyword in front of it

---
+++ src/various/Gfprintf.m
@@ -0,0 +1,53 @@
+%% function for printing information on the command window (based on fprintf)
+%% L. LAURENT -- 12/09/2016 -- luc.laurent@lecnam.net
+
+%     GRENAT - GRadient ENhanced Approximation Toolbox
+%     A toolbox for generating and exploiting gradient-enhanced surrogate models
+%     Copyright (C) 2016  Luc LAURENT <luc.laurent@lecnam.net>
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+%
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+function nbT = Gfprintf(varargin)
+
+%adding text in front of the original text
+txtAdd='GRENAT';
+
+%check if the first argument is a double (corresponding to a file id)
+if isa(varargin{1},'double')
+    %argOk=varargin{2:end};
+    %use the classical fprintf function
+    nbT=fprintf(varargin{:});
+else
+    argOk=varargin;   
+    
+    %convert all inputs to a string
+    str = sprintf(argOk{:});
+    
+    %find new lines
+    strSplit=regexp(str,'\n','split');
+    
+    % display text and adding new keyword
+    nbT=0;
+    for itS=1:numel(strSplit)
+        txtD=[ txtAdd ' | ' strSplit{itS}];
+        nbytes=fprintf(txtD);
+        nbT=nbT+nbytes;
+        if itS<numel(strSplit)
+            nbytes=fprintf('\n');
+            nbT=nbT+nbytes;
+        end
+    end
+end
+end

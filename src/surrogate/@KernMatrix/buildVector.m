3ca6bc9515592dd03a0077ed72a45f210ff549ec
Luc <l2@crans.org>
2017-08-10 15:32:16 +0200

    Add method for building kernel vector at sample points

---
+++ src/surrogate/@KernMatrix/buildVector.m
@@ -0,0 +1,51 @@
+function [V,Vd,Vdd]=buildVector(obj,samplePts,paraV)
+%obj.init;
+%changing the values of the internal parameters
+if nargin>1;obj.paraVal=paraV;end
+%depending on the number of output arguments
+if nargout>1||obj.forceGrad;obj.computeD=true;end
+%compute distance
+distS=repmat(samplePts,obj.nS,1)-obj.sampling;
+%
+fctK=obj.fctKern;
+pVl=obj.paraVal;
+%if derivatives required
+if obj.computeD
+    %if parallel workers are available
+    if obj.parallelOk
+                %                         parfor ii=1:ns
+        %                             %Building by column & evaluation of the Kernel function
+        %                             [V(,dev,ddev]=multiKernel(fctK,dC(:,ii),pVl);
+        %                             %classical part
+        %                             KK(:,ii)=ev;
+        %                             %part of the first derivatives
+        %                             KKa{ii}=dev;
+        %                             %part of the second derivatives
+        %                             KKi{ii}=reshape(ddev,np,ns*np);
+        %                         end
+        %                         %Reordering matrices
+        %                         KKd=horzcat(KKa{:});
+        %                         KKdd=vertcat(KKi{:});
+    else
+        %evaluation of the kernel function for all inter-points
+        %keyboard
+        [V,Vd,Vdd]=multiKernel(fctK,distS,pVl);
+    end
+else
+    if obj.parallelOk
+        %REWRITE
+        %                         %%%%%% PARALLEL %%%%%%
+        %                         %classical kernel matrix by column
+        %                         KK=zeros(ns,ns);
+        %                         parfor ii=1:ns
+        %                             % evaluate kernel function
+        %                             [ev]=multiKernel(fctK,dC(:,ii),pVl);
+        %                             % kernel matrix by column
+        %                             KK(:,ii)=ev;
+        %                         end
+    else
+        % evaluate kernel function
+        [V]=multiKernel(fctK,dC,pVl);
+    end
+end
+end

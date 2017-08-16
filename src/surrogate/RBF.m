4abb18f8a7bf284c5ac4b81e26f854ee0c13c294
Luc <l2@crans.org>
2017-08-17 00:48:55 +0200

    Continue to developp the RBF class

--- src/surrogate/RBF.m
+++ src/surrogate/RBF.m
@@ -27,7 +27,6 @@ classdef RBF < handle
         grad=[];            % sample gradients
         %
         missData;           % class for missing data
-        krgLS;              % class for polynomial regression
         kernelMatrix;       % class for kernel matrix
         metaData;           % class for parameter of the metamodels
         %
@@ -38,9 +37,7 @@ classdef RBF < handle
         YYtot=[];           % full vector of responses and gradients
         K=[];               % kernel matrix
         %
-        beta=[];            % regressors of the kriging generalized Least-Square
-        gamma=[];           % coefficients of the stochastic prt of the kriging
-        sig2=[];            % variance of the Gaussian Process
+        W=[];               % coefficients of RBF/GRBF
         %
         polyOrder=0;        % polynomial order
         kernelFun='sexp';   % kernel function
@@ -89,14 +86,13 @@ classdef RBF < handle
     
     methods
         %% Constructor
-        function obj=KRG(samplingIn,respIn,gradIn,orderIn,kernIn,varargin)
+        function obj=RBF(samplingIn,respIn,gradIn,kernIn,varargin)
             %load data
             obj.sampling=samplingIn;
             obj.resp=respIn;
             if nargin>2;obj.grad=gradIn;end
-            if nargin>3;obj.polyOrder=orderIn;end
-            if nargin>4;obj.kernelFun=kernIn;end
-            if nargin>5;obj.manageOpt(varargin);end
+            if nargin>3;obj.kernelFun=kernIn;end
+            if nargin>4;obj.manageOpt(varargin);end
             %if everything is ok then train
             obj.train();
         end
@@ -139,7 +135,7 @@ classdef RBF < handle
             end
         end
         
-        %% getter for GKRG building
+        %% getter for GRBF building
         function flagG=get.flagG(obj)
             flagG=~isempty(obj.grad);
         end
@@ -220,10 +216,10 @@ classdef RBF < handle
             end
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %Improve condition number of the KRG/GKRG Matrix
+            %Improve condition number of the RBF/RBF Matrix
             if obj.metaData.recond
-                %coefficient for reconditionning (co)kriging matrix
-                coefRecond=(10+size(obj.krgLS.XX,1))*eps;
+                %coefficient for reconditionning RBF matrix
+                coefRecond=eps;
                 %
                 obj.K=obj.K+coefRecond*speye(size(obj.K));
             end
@@ -242,106 +238,54 @@ classdef RBF < handle
             %
         end
         
-        %% core of kriging computation using QR factorization
-        function [detK,logDetK]=coreQR(obj)
+        %% core of RBF computation using QR factorization
+        function coreQR(obj)
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %QR factorization
             [obj.matrices.QK,obj.matrices.RK,obj.matrices.PK]=qr(obj.K);
             %
-            diagRK=diag(obj.matrices.RK);
-            detK=abs(prod(diagRK)); %Q is an unitary matrix
-            logDetK=sum(log(abs(diagRK)));
+            obj.matrices.iK=obj.matrices.PK*(obj.matrices.RK\obj.matrices.QK');
+            yQ=obj.matrices.QK'*obj.YYtot;
+            obj.W=obj.matrices.PK*(obj.matrices.RK\yQ);
             %
-            obj.matrices.QtK=obj.matrices.QK';
-            yQ=obj.matrices.QtK*obj.YYtot;
-            fctQ=obj.matrices.QtK*obj.krgLS.XX;
-            obj.matrices.fcK=obj.krgLS.XX'*obj.matrices.PK/obj.matrices.RK;
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %compute beta coefficient
-            obj.matrices.fcCfct=obj.matrices.fcK*fctQ;
-            block2=obj.matrices.fcK*yQ;
-            obj.beta=obj.matrices.fcCfct\block2;
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %compute gamma coefficient
-            obj.gamma=obj.matrices.PK*(obj.matrices.RK\(yQ-fctQ*obj.beta));
         end
-        %% core of kriging computation using LU factorization
-        function [detK,logDetK]=coreLU(obj)
+        %% core of RBF computation using LU factorization
+        function coreLU(obj)
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %LU factorization
             [obj.matrices.LK,obj.matrices.UK,obj.matrices.PK]=lu(obj.K,'vector');
             %
-            diagUK=diag(obj.matrices.UK);
-            detK=prod(diagUK); %L is a quasi-triangular matrix and contains ones on the diagonal
-            logDetK=sum(log(abs(diagUK)));
+            obj.matrices.iK=obj.matrices.UK\(obj.matrices.LK\obj.matrices.PK);
+            yL=obj.matrices.LK\obj.matrices.PK*obj.YYtot;
+            obj.W=obj.matrices.UK\yL;
             %
-            yP=obj.YYtot(obj.matrices.PK,:);
-            fctP=obj.krgLS.XX(obj.matrices.PK,:);
-            yL=obj.matrices.LK\yP;
-            fctL=obj.matrices.LK\fctP;
-            obj.matrices.fcU=obj.krgLS.XX'/obj.matrices.UK;
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %compute beta coefficient
-            obj.matrices.fcCfct=obj.matrices.fcU*fctL;
-            block2=obj.matrices.fcU*yL;
-            obj.beta=obj.matrices.fcCfct\block2;
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %compute gamma coefficient
-            obj.gamma=obj.matrices.UK\(yL-fctL*obj.beta);
         end
-        %% core of kriging computation using Cholesky (LL) factorization
-        function [detK,logDetK]=coreLL(obj)
+        %% core of RBF computation using Cholesky (LL) factorization
+        function coreLL(obj)
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %Cholesky's fatorization
             %%% to be degugged
             obj.matrices.LK=chol(obj.K,'lower');
             %
-            diagLK=diag(obj.matrices.LK);
-            detK=prod(diagLK)^2;
-            logDetK=2*sum(log(abs(diagLK)));
-            %
-            LtK=obj.matrices.LK';
+            obj.matrices.iK=obj.matrices.LK'\inv(obj.matrices.LK);
             yL=obj.matrices.LK\obj.YYtot;
-            fctL=obj.matrices.LK\obj.krgLS.XX;
-            obj.matrices.fcL=obj.krgLS.XX'/LtK;
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %compute beta coefficient
-            obj.matrices.fcCfct=obj.matrices.fcL*fctL;
-            block2=obj.matrices.fcL*yL;
-            obj.beta=obj.matrices.fcCfct\block2;
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %compute gamma coefficient
-            obj.gamma=LtK\(yL-fctL*obj.beta);
+            obj.W=obj.matrices.LK'\yL;
+            %
+            
         end
-        %% core of kriging computation using no factorization
-        function [detK,logDetK]=coreClassical(obj)
+        %% core of RBF computation using no factorization
+        function coreClassical(obj)
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %classical approach
-            eigVal=eig(obj.K);
-            detK=prod(eigVal);
-            logDetK=sum(log(eigVal));
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %compute gamma and beta coefficients
-            obj.matrices.fcC=obj.krgLS.XX'/obj.K;
-            obj.matrices.fcCfct=obj.matrices.fcC*obj.krgLS.XX;
-            block2=((obj.krgLS.XX'/obj.K)*obj.YYtot);
-            obj.beta=obj.matrices.fcCfct\block2;
-            obj.gamma=obj.K\(obj.YYtot-obj.krgLS.XX*obj.beta);
+            obj.W=obj.K\obj.YYtot;
         end
         
-        %% build factorization, solve the kriging problem and evaluate the log-likelihood
-        function [detK,logDetK]=compute(obj,paraValIn)
+        %% build factorization, solve the RBF problem
+        function compute(obj,paraValIn)
             if nargin==1;
                 paraValIn=obj.paraVal;
             else
@@ -356,45 +300,15 @@ classdef RBF < handle
                 %Factorization of the matrix
                 switch obj.factK
                     case 'QR'
-                        [detK,logDetK]=obj.coreQR;
+                        obj.coreQR;
                     case 'LU'
-                        [detK,logDetK]=obj.coreLU;
+                        obj.coreLU;
                     case 'LL'
-                        [detK,logDetK]=obj.coreLL;
+                        obj.coreLL;
                     otherwise
-                        [detK,logDetK]=obj.coreClassical;
+                        obj.coreClassical;
                 end
                 %
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                %variance of the Gaussian process
-                sizeK=size(obj.K,1);
-                obj.sig2=1/sizeK*...
-                    ((obj.YYtot-obj.krgLS.XX*obj.beta)'*obj.gamma);
-            end
-        end
-        
-        %% function evaluate likelihood
-        function [logLi,Li,liSack]=likelihood(obj,paraValIn)
-            if nargin==1;paraValIn=obj.paraVal;end
-            %compute matrices
-            [detK,logDetK]=obj.compute(paraValIn);
-            %size of the kernel matrix
-            sizeK=size(obj.K,1);
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %computation of the log-likelihood (Jones 1993 / Leary 2004)
-            logLi=sizeK/2*log(2*pi*obj.sig2)+1/2*logDetK+sizeK/2;
-            if nargout>=2
-                %computation of the likelihood (Jones 1993 / Leary 2004)
-                Li=1/((2*pi*obj.sig2)^(sizeK/2)*sqrt(detK))*exp(-sizeK/2);
-            end
-            %computation of the log-likelihood from Sacks 1989
-            if nargout==3
-                liSack=abs(detK)^(1/sizeK)*obj.sig2;
-            end
-            if isinf(logLi)||isnan(logLi)
-                logLi=1e16;
             end
         end
         
@@ -425,38 +339,17 @@ classdef RBF < handle
             %%% Adaptation of the Rippa's method (Rippa 1999/Fasshauer 2007) form M. Bompard (Bompard 2011)
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %coefficient of (co)Kriging
-            coefKRG=obj.gamma;
-            %partial extraction of the diagonal of the inverse of the kernel matrix
-            switch obj.factK
-                case 'QR'
-                    fcC=obj.matrices.fcK*obj.matrices.QtK;
-                    diagMK=diag(obj.matrices.RK\obj.matrices.QtK)-...
-                        diag(fcC'*(obj.matrices.fcCfct\fcC));
-                case 'LU'
-                    fcC=obj.matrices.fcU/obj.matrices.LK;
-                    diagMK=diag(obj.matrices.UK\inv(obj.matrices.LK))-...
-                        diag(fcC'*(obj.matrices.fcCfct\fcC));
-                case 'LL'
-                    fcC=obj.matrices.fcL/obj.matrices.LK;
-                    diagMK=diag(obj.matrices.LK\inv(obj.matrices.LK))-...
-                        diag(fcC'*(obj.matrices.fcCfct\fcC));
-                otherwise
-                    diagMK=diag(inv(obj.K))-...
-                        diag(obj.matrices.fcC'*(obj.matrices.fcCfct\obj.matrices.fcC));
-            end
+            %coefficient of (G)RBF
+            coefRBF=obj.W;
+            
             %vectors of the distances on removed sample points (reponses and gradients)
             % formula from Rippa 1999/Dubrule 1983/Zhang 2010/Bachoc 2011
-            esI=coefKRG./diagMK;
+            esI=coefRBF./diag(obj.matrice.iK);
             esR=esI(1:ns);
             if availGrad;esG=esI(ns+1:end);end
             %responses at the removed sample points
             cv.cvZR=esR-obj.resp;
             cv.cvZ=esR-obj.resp;
-            %vectors of the variance on removed sample points
-            evI=obj.sig2./diagMK;
-            evR=evI(1:ns);
-            if obj.flagG;evG=evI(ns+1:end);end
             
             %computation of the LOO criteria (various norms)
             switch obj.normLOO
@@ -628,13 +521,9 @@ classdef RBF < handle
         
         %% Estimate internal parameters
         function estimPara(obj)
-            switch obj.metaData.estim.type
-                case 'logli'
-                    fun=@(x)obj.likelihood(x);
-                case 'cv'
-                    fun=@(x)obj.cv(x,'estim');
-            end
-            
+            %objective function for hyperparameters estimation
+            fun=@(x)obj.cv(x,'estim');
+%
             obj.paraEstim=EstimPara(obj.nP,obj.metaData,fun);
             obj.lVal=obj.paraEstim.l.Val;
             obj.paraVal=obj.paraEstim.Val;
@@ -668,8 +557,6 @@ classdef RBF < handle
             obj.YYD=der;
             %
             obj.YYtot=[YYT;obj.YYD];
-            %build regression operators
-            obj.krgLS=xLS(obj.sampling,obj.resp,obj.grad,obj.polyOrder,obj.missData);
             %initialize kernel matrix
             obj.kernelMatrix=KernMatrix(obj.kernelFun,obj.sampling,obj.getParaVal);
         end
@@ -696,8 +583,6 @@ classdef RBF < handle
             obj.YYD=[obj.YYD;der];
             %
             obj.YYtot=[obj.YY;obj.YYD];
-            %update regressor operator
-            obj.krgLS.update(samplingIn,respIn,gradIn,obj.missData);
             %initialize kernel matrix
             obj.kernelMatrix.updateMatrix(samplingIn);
         end
@@ -777,9 +662,9 @@ classdef RBF < handle
             np=obj.nP;
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %% KRG/GKRG
+            %% RBF/GRBF
             %%compute response provided by the metamodel at the non sample point
-            %definition des dimensions of the matrix/vector for KRG or GKRG
+            %definition des dimensions of the matrix/vector for RBF or GRBF
             if obj.flagG
                 sizeMatVec=ns*(np+1);
             else
@@ -839,63 +724,33 @@ classdef RBF < handle
                     end
                 end
             end
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %regression matrix at the non-sample points
-            if calcGrad
-                [ff,jf]=obj.krgLS.buildMatrixNonS(X);
-            else
-                [ff]=obj.krgLS.buildMatrixNonS(X);
-            end
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %evaluation of the surrogate model at point X
-            trZ=ff*obj.beta;
-            stoZ=rr'*obj.gamma;
-            Z=trZ+stoZ;
+            Z=rr*obj.W;
             if calcGrad
                 %%verif in 2D+
-                trGZ=jf*obj.beta;
-                stoGZ=jr'*obj.gamma;
-                GZ=trGZ+stoGZ;
+                GZ=jr*obj.W;
             end
             %compute variance
             if nargout >=3
-                variance=obj.computeVariance(rr,ff);
+                variance=obj.computeVariance(rr);
             end
         end
         
         %% compute MSE
-        function variance=computeVariance(obj,rr,ff)
+        function variance=computeVariance(obj,rr)
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %compute the prediction variance (MSE) (Lophaven, Nielsen & Sondergaard
-            %2004 / Marcelet 2008 / Chauvet 1999)
+            %compute variance of the surrogate model (Bompard 2011,Sobester 2005, Gibbs 1997)
             if ~dispWarning;warning off all;end
-            %depending on the factorization
-            switch obj.factKK
-                case 'QR'
-                    rrP=rr'*obj.PK;
-                    Qrr=obj.QtK*rr;
-                    u=obj.fcR*Qrr-ff';
-                    variance=obj.sig2*(1-(rrP/obj.RK)*Qrr+...
-                        u'/obj.fcCfct*u);
-                case 'LU'
-                    rrP=rr(obj.PK,:);
-                    Lrr=obj.LK\rrP;
-                    u=obj.fcU*Lrr-ff';
-                    variance=obj.sig2*(1-(rr'/obj.UK)*Lrr+...
-                        u'/obj.fcCfct*u);
-                case 'LL'
-                    Lrr=obj.LK\rr;
-                    u=obj.fcL*Lrr-ff';
-                    variance=obj.sig2*(1-(rr'/obj.LtK)*Lrr+...
-                        u'/obj.fcCfct*u);
-                otherwise
-                    rKrr=obj.KK \ rr;
-                    u=obj.fc*rKrr-ff';
-                    variance=obj.sig2*(1+u'/obj.fcCfct*u - rr'*rKrr);
-            end
+            %correction for taking into account gradients (debug ....)
+            rrb=rr;
+    if obj.flagG
+        rrb(ns+1:end)=-rrb(ns+1:end);
+    end
+            %
+            variance=1-rr*(obj.matrices.iK*rrb');
             if ~dispWarning;warning on all;end
         end
         
@@ -908,13 +763,11 @@ classdef RBF < handle
                     % Display Building information
                     textd='++ Type: ';
                     textf='';
-                    Gfprintf('\n%s\n',[textd 'Kriging ((G)KRG)' textf]);
+                    Gfprintf('\n%s\n',[textd 'Radial Basis Function ((G)RBF)' textf]);
                     %
                     Gfprintf('>>> Building : ');
-                    dispTxtOnOff(obj.flagG,'GKRG','KRG',true);
+                    dispTxtOnOff(obj.flagG,'GRBF','RBF',true);
                     Gfprintf('>> Kernel function: %s\n',obj.kernelFun);
-                    Gfprintf('>> Deg : %i ',obj.polyOrder);
-                    dispTxtOnOff(obj.polyOrder==0,'(Ordinary)','(Universal)',true);
                     %
                     if dispTxtOnOff(obj.metaData.cv.on,'>> CV: ',[],true)
                         dispTxtOnOff(obj.metaData.cv.full,'>> Computation all CV criteria: ',[],true);
@@ -950,11 +803,11 @@ classdef RBF < handle
                     %
                     Gfprintf('\n');
                 case {'update'}
-                    Gfprintf(' ++ Update KRG\n');
+                    Gfprintf(' ++ Update RBF\n');
                 case {'cv','CV'}
                     Gfprintf(' ++ Run final Cross-Validation\n');
                 case {'end','End','END'}
-                    Gfprintf(' ++ END building KRG\n');
+                    Gfprintf(' ++ END building RBF\n');
             end
         end
     end

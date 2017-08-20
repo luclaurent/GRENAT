50ce47801bd1a6775b208a56a493a87629dee862
Luc <l2@crans.org>
2017-08-20 17:31:26 +0200

    Extract static function from KernMatrix class

--- src/kernANDfun/@KernMatrix/KernMatrix.m
+++ src/kernANDfun/@KernMatrix/KernMatrix.m
@@ -215,40 +215,7 @@ classdef KernMatrix < handle
             dispTableTwoColumns(obj.listKernel,obj.listKernelTxt)
         end
         
-        %add new sample points
+        % 
         flag=addSample(obj,newS)
     end
 end
-
-
-
-%% function display table with two columns of text
-function dispTableTwoColumnsStruct(tableFieldIn,structIn)
-%size of every components in tableA
-sizeA=cellfun(@numel,tableFieldIn);
-maxA=max(sizeA);
-%space after each component
-spaceA=maxA-sizeA+3;
-spaceTxt=' ';
-%display table
-for itT=1:numel(tableFieldIn)
-    if isfield(structIn,tableFieldIn{itT})
-        fprintf('%s%s%s\n',tableFieldIn{itT},spaceTxt(ones(1,spaceA(itT))),structIn.(tableFieldIn{itT}));
-    end
-end
-end
-
-%% function display table with two columns of text
-function dispTableTwoColumns(tableA,tableB)
-%size of every components in tableA
-sizeA=cellfun(@numel,tableA);
-maxA=max(sizeA);
-%space after each component
-spaceA=maxA-sizeA+3;
-spaceTxt=' ';
-%display table
-for itT=1:numel(tableA)
-    fprintf('%s%s%s\n',tableA{itT},spaceTxt(ones(1,spaceA(itT))),tableB{itT});
-end
-end
-

fff2f9480d4aeada13e243e2c60ca8b061a71529
Luc <l2@crans.org>
2017-08-22 17:29:03 +0200

    Restore MissingData method for GRENAT

--- @GRENAT/train.m
+++ @GRENAT/train.m
@@ -1,22 +1,47 @@
-       %train the metamodel
-        function train(obj)
-             %check if data are missing
-            checkMissingData(obj);
-            %store normalization data
-            obj.confMeta.norm=obj.norm;
-            %train surrogate model
-            obj.dataTrain=BuildMeta(obj.samplingN,obj.respN,obj.gradN,obj.confMeta);
-            %save estimate parameters
-            if isfield(obj.dataTrain.build,'para');
-                obj.confMeta.definePara(obj.dataTrain.build.para);
-                obj.confMeta.updatePara;
-            end
-            %change state of flags
-            obj.runTrain=false;
-            obj.runErr=true;
-            
-            % keyboard
-            % if metaData.norm.on&&~isempty(metaData.norm.resp.std)
-            %     ret.build.sig2=ret.build.sig2*metaData.norm.resp.std^2;
-            % end
-        end
+%% Method of GRENAT class
+% L. LAURENT -- 26/06/2016 -- luc.laurent@lecnam.net
+
+%     GRENAT - GRadient ENhanced Approximation Toolbox
+%     A toolbox for generating and exploiting gradient-enhanced surrogate models
+%     Copyright (C) 2016-2017  Luc LAURENT <luc.laurent@lecnam.net>
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+%
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%% Train the metamodel
+% INPUTS:
+% - none
+% OUTPUTS:
+% - none
+
+function train(obj)
+%check if data are missing
+checkMissingData(obj);
+%store normalization data
+obj.confMeta.norm=obj.norm;
+%train surrogate model
+obj.dataTrain=BuildMeta(obj.samplingN,obj.respN,obj.gradN,obj.confMeta);
+%save estimate parameters
+if isfield(obj.dataTrain.build,'para');
+    obj.confMeta.definePara(obj.dataTrain.build.para);
+    obj.confMeta.updatePara;
+end
+%change state of flags
+obj.runTrain=false;
+obj.runErr=true;
+
+% keyboard
+% if metaData.norm.on&&~isempty(metaData.norm.resp.std)
+%     ret.build.sig2=ret.build.sig2*metaData.norm.resp.std^2;
+% end
+end

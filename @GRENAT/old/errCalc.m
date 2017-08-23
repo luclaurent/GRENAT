87bc1c4471ed171607e9546fc19ffb95324bf44c
Luc <l2@crans.org>
2017-08-23 13:18:39 +0200

    GRENAT: restore method for compute errors

--- @GRENAT/old/errCalc.m
+++
@@ -1,6 +0,0 @@
-       %compute and show the errors of the metamodel (using reference if it is
-        %available)
-        function errCalc(obj)
-            obj.err=critErrDisp(obj.nonsampleResp,obj.respRef,obj.dataTrain.build);
-            obj.runErr=false;
-        end
